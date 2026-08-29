081c9b7d872fe3bca89899238d0ca061679b113c
Adarsh Raj <itsadarshraj17@gmail.com>
2026-08-29 22:51:00 +0530

    Backend URL update

--- frontend-chat/src/config/AxiosHelper.js
+++ frontend-chat/src/config/AxiosHelper.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-export const baseURL = 'http://localhost:8080';
+export const baseURL = import.meta.env.VITE_API_URL;
 
 const httpClient = axios.create({
-    baseURL: baseURL,
+    baseURL,
 });
 
 export default httpClient;
